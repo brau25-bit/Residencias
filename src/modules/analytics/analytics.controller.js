6208f862ca21048c9f28fcf08e1a542315c379e7
brau25-bit <brauliochaveza@gmail.com>
2026-01-01 14:41:33 -0600

    Analytics controller y service

---
+++ src/modules/analytics/analytics.controller.js
@@ -0,0 +1,59 @@
+import { AnalyticsService } from "./analytics.service";
+
+export class AnalyticsController{
+    static async getReportsByStatus(req, res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async getReportsByCategory(req, res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async getReportsOverTime(req, res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async getReportHeatMap(req, res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async getReportResolutionTime(req, res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async getTimeByStatus(req, res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async getReportStatusTransition(req, res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
---
+++ src/modules/analytics/analytics.service.js
@@ -0,0 +1,59 @@
+import prisma from "../../db/client.js";
+
+export class AnalyticsService{
+    static async getReportsByStatus(req, res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async getReportsByCategory(req, res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async getReportsOverTime(req, res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async getReportHeatMap(req, res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async getReportResolutionTime(req, res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async getTimeByStatus(req, res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async getReportStatusTransition(req, res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
